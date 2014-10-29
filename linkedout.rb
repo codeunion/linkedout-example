cae17786f405fce7f4783070515f708ac0e50fdc
Tanner Welsh <tannerwelsh@gmail.com>
2014-10-29 19:58:06 -0400

    Can add jobs from résumé show page; edit partial names

--- linkedout.rb
+++ linkedout.rb
@@ -40,6 +40,16 @@ put "/users/edit" do
   redirect "/"
 end
 
+post "/jobs" do
+  job_attrs = params[:job]
+  job_attrs.merge!({ :user => default_user })
+
+  job = Job.new(job_attrs)
+  job.save
+
+  redirect "/"
+end
+
 put "/jobs/edit" do
   job_attrs = params[:job]
 
