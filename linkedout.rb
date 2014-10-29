fbbe4e351a765d52efa66735faf427d37dfc69c4
Tanner Welsh <tannerwelsh@gmail.com>
2014-10-29 12:02:46 -0400

    Add helper method to retrieve a default user
    Moving the decision of "who should the default user be?" into its own
    method encapsulates that logic and makes it easier to reason about and
    change in the future.

--- linkedout.rb
+++ linkedout.rb
@@ -3,10 +3,15 @@ require 'sinatra'
 require_relative 'config/dotenv'
 require_relative 'models'
 
+helpers do
+  def default_user
+    @default_user ||= User.last
+  end
+end
+
 get "/" do
-  @user = User.last
-  @jobs = @user.jobs
-  @skills = @user.skills
+  @jobs = default_user.jobs
+  @skills = default_user.skills
 
   erb :'resumes/show'
 end
