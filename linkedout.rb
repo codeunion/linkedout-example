0902118634b3d34eaf1de3cd5959d0469dbb308c
Tanner Welsh <tannerwelsh@gmail.com>
2014-10-29 12:53:37 -0400

    User can edit job information at /resumes/edit
    The résumé edit page will now render all of the jobs as forms that users
    can update. As of now, each job is editable in isolation (i.e. no batch
    updates of multiple jobs).
    
    Added a new route to handle updating jobs.

--- linkedout.rb
+++ linkedout.rb
@@ -23,6 +23,8 @@ get "/" do
 end
 
 get "/resumes/edit" do
+  @jobs = default_user.jobs
+
   erb :'resumes/edit'
 end
 
@@ -30,6 +32,17 @@ put "/users/edit" do
   user_attrs = params[:user]
 
   default_user.update(user_attrs)
+
+  redirect "/"
+end
+
+put "/jobs/edit" do
+  job_attrs = params[:job]
+
+  job_id = job_attrs.delete("id")
+
+  job = Job.get(job_id)
+  job.update(job_attrs)
 
   redirect "/"
 end
