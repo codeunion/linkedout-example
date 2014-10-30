b3fe17b61748979b9ca70a5fca25e7acbb1ee4be
Tanner Welsh <tannerwelsh@gmail.com>
2014-10-30 00:56:22 -0400

    Refactor main.js to use named functions
    Better to break up the work into separate functions with explicit
    purposes. +1 code style points.

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,15 @@
-// Wait to execute all code until the document is ready
-// (i.e. all of the DOM nodes have been loaded)
-$(document).ready(function() {
-
-  // Select the new skill form using its name attribute
-  var $newSkillForm = $('form[name="new_skill"]');
+var createNewSkillsOnSubmit = function() {
+  var $newSkillForm = getNewSkillForm();
 
   // Bind to the "submit" event of the new skill form
   $newSkillForm.submit(function (evt) {
-
     // Prevent the submit from sending an HTTP POST request
     // Instead, we'll handle the request with AJAX
     evt.preventDefault();
 
     // Grab the form data and serialize it as a string in
     // standard URL-encoded notation
-    var newSkillData = $(this).serialize();
+    var skillFormData = $(this).serialize();
 
     // Send a POST request asynchronously to the "/skills"
     // route on the server, passing the serialized form data
@@ -24,21 +19,34 @@ $(document).ready(function() {
     // this will send a request to the server to save this
     // skill to the database
     //
-    // The anonymous function (third argument passed to $.post)
-    // is executed when the browser receives a response from
+    // The insertNewSkillIntoDOM function (argument 3) will
+    // be executed when the browser receives a response from
     // the server. It is passed the response body, which in
     // this case is a snippet of HTML representing the newly-
     // created skill.
-    $.post("/skills", newSkillData, function(newSkillHTML) {
-
-      // Add the new skill to the list, just before the form's
-      // parent 'li' element
-      $newSkillForm.parent('li')
-                   .before(newSkillHTML);
-
-      // Reset the form so that new skills can be added
-      $newSkillForm.get(0).reset();
-
-    });
+    $.post("/skills", skillFormData, insertNewSkillIntoDOM);
   });
+};
+
+var getNewSkillForm = function() {
+  // Select the new skill form using its name attribute
+  return $('form[name="new_skill"]');
+};
+
+var insertNewSkillIntoDOM = function(newSkillHTML) {
+  var $newSkillForm = getNewSkillForm();
+
+  // Add the new skill to the list, just before the form's
+  // parent 'li' element
+  $newSkillForm.parent('li').before(newSkillHTML);
+
+  // Reset the form so that new skills can be added
+  $newSkillForm.get(0).reset();
+};
+
+
+// Wait to execute all code until the document is ready
+// (i.e. all of the DOM nodes have been loaded)
+$(document).ready(function() {
+  createNewSkillsOnSubmit();
 });
