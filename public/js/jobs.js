6b47f9f088d8527b37c9f4f7a3d71d865314c8c4
Tanner Welsh <tannerwelsh@gmail.com>
2014-11-01 15:16:46 -0400

    Add logging to jobs.js
    If you want to see what JavaScript is doing, simply open your browser
    console. As you click around to create, edit, and delete jobs, logging
    information will render to the JavaScript console.

--- public/js/jobs.js
+++ public/js/jobs.js
@@ -1,11 +1,24 @@
+// A utility function to log information to the console
+var log = function(message) {
+  var timestamp = (new Date()).toTimeString();
+  console.log('[' + timestamp + ']  ' + message);
+};
+
 var createJobOnSubmit = function() {
   $('.jobs').on('submit', 'form[name="new_job"]', function(evt) {
+    log("New job form submitted");
+
     evt.preventDefault();
 
     var $newJobForm = $(this);
     var newJobFormData = $(this).serialize();
 
+    log("Sending POST request to /jobs");
+
     $.post("/jobs", newJobFormData, function(jobHTML) {
+      log("Received response from POST request to /jobs");
+
+      log("Adding new job element to list");
       $newJobForm.parent('li').before(jobHTML);
 
       $newJobForm.get(0).reset();
@@ -15,11 +28,14 @@ var createJobOnSubmit = function() {
 
 var showEditJobFormOnClick = function() {
   $('.jobs').on('click', '.js_edit_job', function(evt) {
+    log("Show edit job form link clicked");
+
     evt.preventDefault();
 
     var $jobElem = $(this).parent('.job');
     var $editJobForm = $jobElem.siblings('form[name="edit_job"]');
 
+    log("Hiding job element and showing job form");
     $jobElem.addClass('hidden');
     $editJobForm.removeClass('hidden');
   });
@@ -27,6 +43,8 @@ var showEditJobFormOnClick = function() {
 
 var updateJobOnSubmit = function() {
   $('.jobs').on('submit', 'form[name="edit_job"]', function(evt) {
+    log("Edit job form submitted");
+
     evt.preventDefault();
 
     var $editJobForm = $(this);
@@ -36,12 +54,15 @@ var updateJobOnSubmit = function() {
     // 'job' in the same containing element (in this case, <li>)
     var $jobElem = $editJobForm.siblings('.job');
 
+    log("Sending PUT request to /jobs/edit");
+
     // Send async PUT request to /jobs/edit
     $.ajax({
       url: '/jobs/edit',
       type: 'PUT',
       data: editJobFormData
     }).done(function(responseData) {
+      log("Received response from PUT request to /jobs/edit");
       // This function will execute when the response comes
       // back from the server
       //
@@ -49,9 +70,11 @@ var updateJobOnSubmit = function() {
       // (as a <div> element with job info inside)
       var newJobHTML = responseData;
 
+      log("Hiding job form and showing job element");
       $jobElem.removeClass('hidden');
       $editJobForm.addClass('hidden');
 
+      log("Replacing old job info with updated info");
       $jobElem.replaceWith(newJobHTML);
     });
   });
@@ -59,6 +82,8 @@ var updateJobOnSubmit = function() {
 
 var deleteJobOnSubmit = function() {
   $('.jobs').on('submit', 'form[name="delete_job"]', function(evt) {
+    log("Edit job form submitted");
+
     evt.preventDefault();
 
     var $deleteJobForm = $(this);
@@ -68,6 +93,8 @@ var deleteJobOnSubmit = function() {
     // it when the delete request completes
     var $jobContainerElem = $(this).closest('li');
 
+    log("Sending DELETE request to /jobs");
+
     // Sending a DELETE request requires using the jQuery
     // .ajax() function and configuring the url, type,
     // data, and complete options
@@ -76,6 +103,9 @@ var deleteJobOnSubmit = function() {
       type: 'DELETE',
       data: deleteJobFormData
     }).done(function() {
+      log("Received response from DELETE request to /jobs");
+
+      log("Removing deleted job element");
       $jobContainerElem.remove();
     });
   });
