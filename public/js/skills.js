c9c3821c569de3cf1ea7e7bece7ad61c08eb646a
Tanner Welsh <tannerwelsh@gmail.com>
2014-10-31 20:19:34 -0400

    Separate JS into two files: jobs.js and skills.js
    That main.js file was getting much too large. Time to split it into
    smaller scripts that manage jobs and skills, respectively.

--- public/js/skills.js
+++ public/js/skills.js
@@ -47,59 +47,8 @@ var insertNewSkillIntoDOM = function(newSkillHTML) {
   $newSkillForm.get(0).reset();
 };
 
-var createNewJobsOnSubmit = function() {
-  var $newJobForm = getNewJobForm();
-
-  $newJobForm.submit(function (evt) {
-    evt.preventDefault();
-
-    var jobFormData = $(this).serialize();
-
-    $.post("/jobs", jobFormData, insertNewJobIntoDOM);
-  });
-};
-
-var getNewJobForm = function() {
-  return $('form[name="new_job"]');
-};
-
-var insertNewJobIntoDOM = function(newJobHTML) {
-  var $newJobForm = getNewJobForm();
-
-  $newJobForm.parent('li').before(newJobHTML);
-
-  $newJobForm.get(0).reset();
-};
-
-var deleteJobOnSubmit = function() {
-  var $deleteJobForm = $('form[name="delete_job"]');
-
-  $deleteJobForm.submit(function (evt) {
-    evt.preventDefault();
-
-    var jobFormData = $(this).serialize();
-
-    // Grab the parent <li> element so that we can remove
-    // it when the delete request completes
-    var $parentLiElement = $(this).parent('li');
-
-    // Sending a DELETE request requires using the jQuery
-    // .ajax() function and configuring the url, type,
-    // data, and complete options
-    $.ajax({
-      url: '/jobs',
-      type: 'DELETE',
-      data: jobFormData,
-      complete: function() { $parentLiElement.remove(); }
-    });
-  });
-};
-
-
 // Wait to execute all code until the document is ready
 // (i.e. all of the DOM nodes have been loaded)
 $(document).ready(function() {
   createNewSkillsOnSubmit();
-  createNewJobsOnSubmit();
-  deleteJobOnSubmit();
 });
