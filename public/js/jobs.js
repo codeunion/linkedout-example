a333e26144322532b2febe03bef8bdaa004cff37
Tanner Welsh <tannerwelsh@gmail.com>
2014-11-01 14:37:33 -0400

    Refactor jobs.js for consistency and clarity
    - Make naming more conventional
    - Remove smaller functions
    - Standardize format of functions

--- public/js/jobs.js
+++ public/js/jobs.js
@@ -1,34 +1,27 @@
-var createNewJobsOnSubmit = function() {
-  $('.jobs').on('submit', 'form[name="new_job"]',function(evt) {
+var createJobOnSubmit = function() {
+  $('.jobs').on('submit', 'form[name="new_job"]', function(evt) {
     evt.preventDefault();
 
-    var jobFormData = $(this).serialize();
+    var $newJobForm = $(this);
+    var newJobFormData = $(this).serialize();
 
-    $.post("/jobs", jobFormData, insertNewJobIntoDOM);
+    $.post("/jobs", newJobFormData, function(jobHTML) {
+      $newJobForm.parent('li').before(jobHTML);
+
+      $newJobForm.get(0).reset();
+    });
   });
 };
 
-var getNewJobForm = function() {
-  return $('form[name="new_job"]');
-};
-
-var insertNewJobIntoDOM = function(newJobHTML) {
-  var $newJobForm = getNewJobForm();
-
-  $newJobForm.parent('li').before(newJobHTML);
-
-  $newJobForm.get(0).reset();
-};
-
-var showJobEditFormOnClick = function() {
+var showEditJobFormOnClick = function() {
   $('.jobs').on('click', '.js_edit_job', function(evt) {
     evt.preventDefault();
 
     var $jobElem = $(this).parent('.job');
-    var $jobEditForm = $jobElem.siblings('form[name="edit_job"]');
+    var $editJobForm = $jobElem.siblings('form[name="edit_job"]');
 
     $jobElem.addClass('hidden');
-    $jobEditForm.removeClass('hidden');
+    $editJobForm.removeClass('hidden');
   });
 };
 
@@ -36,18 +29,18 @@ var updateJobOnSubmit = function() {
   $('.jobs').on('submit', 'form[name="edit_job"]', function(evt) {
     evt.preventDefault();
 
-    var jobFormData = $(this).serialize();
+    var $editJobForm = $(this);
+    var editJobFormData = $editJobForm.serialize();
 
-    var $jobEditForm = $(this);
     // The job item to update is the the element of class
     // 'job' in the same containing element (in this case, <li>)
-    var $jobListItem = $jobEditForm.siblings('.job');
+    var $jobElem = $editJobForm.siblings('.job');
 
     // Send async PUT request to /jobs/edit
     $.ajax({
       url: '/jobs/edit',
       type: 'PUT',
-      data: jobFormData
+      data: editJobFormData
     }).done(function(responseData) {
       // This function will execute when the response comes
       // back from the server
@@ -56,10 +49,10 @@ var updateJobOnSubmit = function() {
       // (as a <div> element with job info inside)
       var newJobHTML = responseData;
 
-      $jobListItem.removeClass('hidden');
-      $jobEditForm.addClass('hidden');
+      $jobElem.removeClass('hidden');
+      $editJobForm.addClass('hidden');
 
-      $jobListItem.replaceWith(newJobHTML);
+      $jobElem.replaceWith(newJobHTML);
     });
   });
 };
@@ -68,11 +61,12 @@ var deleteJobOnSubmit = function() {
   $('.jobs').on('submit', 'form[name="delete_job"]', function(evt) {
     evt.preventDefault();
 
-    var jobFormData = $(this).serialize();
+    var $deleteJobForm = $(this);
+    var deleteJobFormData = $deleteJobForm.serialize();
 
     // Grab the containing <li> element so that we can remove
     // it when the delete request completes
-    var $parentLiElement = $(this).closest('li');
+    var $jobContainerElem = $(this).closest('li');
 
     // Sending a DELETE request requires using the jQuery
     // .ajax() function and configuring the url, type,
@@ -80,16 +74,17 @@ var deleteJobOnSubmit = function() {
     $.ajax({
       url: '/jobs',
       type: 'DELETE',
-      data: jobFormData,
-      complete: function() { $parentLiElement.remove(); }
+      data: deleteJobFormData
+    }).done(function() {
+      $jobContainerElem.remove();
     });
   });
 };
 
 $(document).ready(function() {
-  createNewJobsOnSubmit();
+  createJobOnSubmit();
 
-  showJobEditFormOnClick();
+  showEditJobFormOnClick();
   updateJobOnSubmit();
 
   deleteJobOnSubmit();
